e5a16214564258d041835fcc742d97b07ea35568
Lance <lance.byrd@gleif.org>
2024-05-12 14:35:01 -0400

    Reg pilot api naming testing (#4)
    * improved normalized responses for status, logging, and naming to reg-pilot-ap
    
    Signed-off-by: 2byrds <2byrds@gmail.com>
    
    * add VERIFIER_REQUESTS config
    
    Signed-off-by: 2byrds <2byrds@gmail.com>
    
    ---------
    
    Signed-off-by: 2byrds <2byrds@gmail.com>

--- tests/regps/app/test_login.py
+++ tests/regps/app/test_login.py
@@ -62,35 +62,3 @@ def test_login():
     assert aid == AID
     assert sig == "0BBbeeBw3lVmQWYBpcFH9KmRXZocrqLH_LZL4aqg5W9-NMdXqIYJ-Sao7colSTJOuYllMXFfggoMhkfpTKnvPhUF"
     assert ser == '"@method": POST\n"@path": /\n"signify-resource": EP4kdoVrDh4Mpzh2QbocUYIv4IjLZLDU367UO0b40f6x\n"signify-timestamp": 2024-05-04T20:20:33.730000+00:00\n"@signature-params: (@method @path signify-resource signify-timestamp);created=1714854033;keyid=BPoZo2b3r--lPBpURvEDyjyDkS65xBEpmpQhHQvrwlBE;alg=ed25519"'
-
-
-# def test_verify_sig():
-#     app = falcon.App()
-#     client = falcon.testing.TestClient(app)
-
-#     result = client.simulate_put(f'/presentations/{SAID}',
-#                                     body=acdc,
-#                                     headers={'Content-Type': 'application/json+cesr'})
-#     assert result.status == falcon.HTTP_202
-    
-#     hby.kevers[hab.pre] = hab.kever
-    
-#     auth = Authorizer(hby, vdb, eccrdntler.rgy.reger, [LEI1])
-#     auth.processPresentations()
-    
-#     result = client.simulate_get(f'/authorizations/{hab.pre}')
-#     assert result.status == falcon.HTTP_OK
-
-#     data = 'this is the raw data'
-#     raw = data.encode("utf-8")
-#     cig = hab.sign(ser=raw, indexed=False)[0]
-#     assert cig.qb64 == '0BChOKVR4b5t6-cXKa3u3hpl60X1HKlSw4z1Rjjh1Q56K1WxYX9SMPqjn-rhC4VYhUcIebs3yqFv_uu0Ou2JslQL'
-#     assert hby.kevers[hab.pre].verfers[0].verify(sig=cig.raw, ser=raw)
-#     result = client.simulate_post(f'/request/verify/{hab.pre}',params={'data': data, 'sig': cig.qb64})
-#     assert result.status == falcon.HTTP_202
-    
-#     data = '"@method": GET\n"@path": /verify/header\n"signify-resource": EHYfRWfM6RxYbzyodJ6SwYytlmCCW2gw5V-FsoX5BgGx\n"signify-timestamp": 2024-05-01T19:54:53.571000+00:00\n"@signature-params: (@method @path signify-resource signify-timestamp);created=1714593293;keyid=BOieebDzg4uaqZ2zuRAX1sTiCrD3pgGT3HtxqSEAo05b;alg=ed25519"'
-#     raw = data.encode("utf-8")
-#     cig = hab.sign(ser=raw, indexed=False)[0]
-#     assert cig.qb64 == '0BB1Z2DS3QvIBdZJ1Q7yuZCUG-6YkVXDm7dcGbIFEIsLYEBfFXk8P_Y9FUACTlv5vCHeCet70QzVdR8fu5tLBKkP'
-#     assert hby.kevers[hab.pre].verfers[0].verify(sig=cig.raw, ser=raw)
