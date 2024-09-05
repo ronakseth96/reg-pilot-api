891f3e1262838a0ee85408cd7c5cb54a732a8703
Lance <lance.byrd@gleif.org>
2024-09-04 21:05:03 -0400

    test forwarded headers (#43)
    Signed-off-by: 2byrds <2byrds@gmail.com>

--- tests/unit/test_verifying.py
+++ tests/unit/test_verifying.py
@@ -63,3 +63,38 @@ def test_verify_cig():
         ser
         == '"@method": POST\n"@path": /\n"signify-resource": EP4kdoVrDh4Mpzh2QbocUYIv4IjLZLDU367UO0b40f6x\n"signify-timestamp": 2024-05-04T20:20:33.730000+00:00\n"@signature-params: (@method @path signify-resource signify-timestamp);created=1714854033;keyid=BPoZo2b3r--lPBpURvEDyjyDkS65xBEpmpQhHQvrwlBE;alg=ed25519"'
     )
+
+def test_verify_forwarded_cig():
+    # AID and SAID should be the same as what is in credential.cesr for the ECR credential
+    # see https://trustoverip.github.io/tswg-acdc-specification/#top-level-fields to understand the fields/values
+    AID = "EP4kdoVrDh4Mpzh2QbocUYIv4IjLZLDU367UO0b40f6x"
+    # SAID = "EElnd1DKvcDzzh7u7jBjsg2X9WgdQQuhgiu80i2VR-gk"
+
+    # See https://datatracker.ietf.org/doc/html/rfc9421#section-4.3 for forwarding signed headers
+    headers = {
+        "HOST": "proxy:3434",
+        "CONNECTION": "keep-alive",
+        "METHOD": "POST",
+        "FORWARDED": "host: localhost:7676",
+        "SIGNATURE": 'indexed="?0";signify="0BBbeeBw3lVmQWYBpcFH9KmRXZocrqLH_LZL4aqg5W9-NMdXqIYJ-Sao7colSTJOuYllMXFfggoMhkfpTKnvPhUF"',
+        "SIGNATURE-INPUT": 'signify=("@method" "@path" "signify-resource" "signify-timestamp");created=1714854033;keyid="BPoZo2b3r--lPBpURvEDyjyDkS65xBEpmpQhHQvrwlBE";alg="ed25519"',
+        "SIGNIFY-RESOURCE": "EP4kdoVrDh4Mpzh2QbocUYIv4IjLZLDU367UO0b40f6x",
+        "SIGNIFY-TIMESTAMP": "2024-05-04T20:20:33.730000+00:00",
+        "ACCEPT": "*/*",
+        "ACCEPT-LANGUAGE": "*",
+        "SEC-FETCH-MODE": "cors",
+        "USER-AGENT": "node",
+        "ACCEPT-ENCODING": "gzip, deflate",
+    }
+    scope = dict(type="http", headers=Headers(headers).raw, method="POST", path="/")
+    req = fastapi.Request(scope)
+    aid, sig, ser = signed_headers_verifier.VerifySignedHeaders.handle_headers(req)
+    assert aid == AID
+    assert (
+        sig
+        == "0BBbeeBw3lVmQWYBpcFH9KmRXZocrqLH_LZL4aqg5W9-NMdXqIYJ-Sao7colSTJOuYllMXFfggoMhkfpTKnvPhUF"
+    )
+    assert (
+        ser
+        == '"@method": POST\n"@path": /\n"signify-resource": EP4kdoVrDh4Mpzh2QbocUYIv4IjLZLDU367UO0b40f6x\n"signify-timestamp": 2024-05-04T20:20:33.730000+00:00\n"@signature-params: (@method @path signify-resource signify-timestamp);created=1714854033;keyid=BPoZo2b3r--lPBpURvEDyjyDkS65xBEpmpQhHQvrwlBE;alg=ed25519"'
+    )
